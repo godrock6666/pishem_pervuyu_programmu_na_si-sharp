4be8896e58d63568ca3fb96aa66846e35821cccf
godrock6666 <godrock6666@gmail.com>
2021-01-19 02:01:04 +0300

    no message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helllo World!");
+            Console.WriteLine("Hel6666llo World!");
         }
     }
 }
