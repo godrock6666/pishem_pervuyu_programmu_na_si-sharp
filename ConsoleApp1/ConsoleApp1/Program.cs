bc25197543a914a1ba2b6a3357fb81ce31e264cc
godrock6666 <godrock6666@gmail.com>
2021-01-19 23:53:36 +0300

    no message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hel6343666llo World!");
+            const double a = 5;
+            const double b = 7;
+            double s, sum, g, gCos;
+            s = (a * b) / 2 * Math.Sin(90 * Math.PI / 180);
+            sum = Math.Pow(a, 2) + Math.Pow(b, 2);
+            g = Math.Sqrt(sum);
+            gCos = Math.Sqrt(sum) - 2 * a * b * Math.Cos(90 * Math.PI / 180);
+
+            Console.WriteLine("Площадь треугольника равна " + s);
+            Console.WriteLine("Длина гипотенузы согласно теоремы пифагора  равна " + g);
+            Console.WriteLine("Длина гипотенузы согласно теореме косинусов равна " + gCos);
+            Console.ReadKey();
         }
     }
 }
