fa3909d9bdd1ceaee5bca9af84ed38969bb021bb
godrock6666 <godrock6666@gmail.com>
2021-04-14 23:58:48 +0300

    no message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,32 +2,23 @@
 
 namespace ConsoleApp1
 {
+    public class TV
+    {
+        public static int Channels;
+        public static int Channel = 1;
+
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
-
-            int value = 1;//ONE
-            for (int i = 0; i < 20; i++)
-                Console.Write($"{value += 3} ");
-            string str; //TWO
-            do
-                str = Console.ReadLine();
-            while (str != "Root");
-            Random sc = new Random();//THREE
-            int[] mas = new int[5];
-            int[] mas_1 = new int[mas.Length];
-            int[] mas_2 = new int[mas.Length];
-            for (int i = 0; i < mas.Length; i++)
-            {
-                mas[i] = sc.Next(0, 10);
-                mas_1[i] = sc.Next(0, 10);
-                mas_2[i] = mas[i] + mas_1[i];
-                Console.WriteLine(mas_2[i]);
-
-
-
-            }
+            string s = "Сегодня мы с вами рассмотрели, как работать со строками" +
+                " в Си-шарп. Были описаны основные операторы и методы, которые используются для работы со строками";
+            s = s.Split('.')[1];
+            s = s.Substring(1, s.IndexOf(",") - 1);
+            Console.WriteLine(s);
         }
     }
 }
